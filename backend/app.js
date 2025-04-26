885918ea85e6ba0f8af898bcccc87f00d97bdfdd
Sobaan Rana <sobaanrana@gmail.com>
2025-04-26 19:14:26 +0100

    places and users controllers and routes added

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+const { default: mongoose } = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api/places", placesRoutes); // => /api/places...
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
+});
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sobaan:helloworld@cluster0.8ccefyx.mongodb.net/users-places_db?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => app.listen(4000))
+  .catch((err) => console.log(err));
